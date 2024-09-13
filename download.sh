43aaf44429dd1bdbdb00a8473ef461e8371e19c0
Jose Geraldo Fernandes macaverde604@gmail.com <macaverde604@gmail.com>
2024-09-13 22:49:45 +0000

    add dataset implementation

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget https://physionet.org/static/published-projects/ludb/lobachevsky-university-electrocardiography-database-1.0.1.zip
+unzip lobachevsky-university-electrocardiography-database-1.0.1.zip
+rm lobachevsky-university-electrocardiography-database-1.0.1.zip
